7f0dddc965466ea37154cc6b475e93b2f7d4f2c0
Roman <cisinskijroman@gmail.com>
2025-04-10 20:48:03 +0300

    add database

--- config/settings.py
+++ config/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'inclusive_map',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password', # very HARD password
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HAckathon',
+        'USER': 'postgres',
+        'PASSWORD': '1201', 
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 
